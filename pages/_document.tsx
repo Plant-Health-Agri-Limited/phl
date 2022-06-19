e7540fdfa9b7026064235f45c3cc5ae902b22190
mich-elo <mchandula17@gmail.com>
2022-06-19 13:33:09 +0200

    components update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>PLant Health Agri Limited</title>
         <meta charSet="UTF-8"/>
         <meta name="description" content="Plant Health Agri Limited is a wholly owned Zambian company focusing on agriculture. PHAL services both the small holder and commercial famers."/>
       </Head>
